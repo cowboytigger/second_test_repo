ae5f8c34e4c3694ec6d513f9ed53f11ba7879bac
cowboytigger <tigger.cowboy@gmail.com>
2024-04-04 16:20:55 +0200

    Add files via upload

---
+++ func.py
@@ -0,0 +1,9 @@
+def say_hi():
+  print("Hello there from the second test repo")
+
+def divide_numbers(x,y):
+  if(y==0): 
+    print("Are you crazy? Who is dividing by 0?")
+  else: 
+    print(x/y)
+  
